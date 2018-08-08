f948cd5e64f1f3f224c5b7073e4f93892dbbfb9a
zhaopengfei1919 <2582418234@qq.com>
2018-08-08 18:32:41 +0800

    设置首页

--- dudu/AppDelegate.m
+++ dudu/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "BaseTableBarControllerView.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window.rootViewController = [[BaseTableBarControllerView alloc]init];
+    [self.window makeKeyWindow];
     return YES;
 }
 
--- dudu/MainView/Home/Controller/HomeViewController.m
+++ dudu/MainView/Home/Controller/HomeViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.title = @"";
     // Do any additional setup after loading the view.
 }
 
